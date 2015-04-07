20c0cced4ef578eb844866fd9b9de8d306bb2fe3
Ronan McHugh <mchugh.r@gmail.com>
2015-04-07 14:00:50 +0200

    before_action is only available in rails 4.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < UmlautController
   include HighVoltage::StaticPage
   include UmlautConfigurable
-  before_action :set_config
+  before_filter :set_config
 
   def set_config
     UmlautConfigurable.set_default_configuration!(umlaut_config)
